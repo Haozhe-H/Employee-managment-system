42bf1a71d8dc517e1cb4e4ee9fc0f79a756effaa
Haozhe-H <haozhehuang@outlook.com>
2023-04-23 21:36:20 -0400

    update prompt questions

--- server.js
+++ server.js
@@ -29,13 +29,91 @@ const afterConnection = () => {
   console.log("|      Employee Manager     |");
   console.log("|                           |");
   console.log("|===========================|");
-//   startPrompt();
+  userPrompt();
 };
 
-// const startPrompt = () => {
-//     inquirer.prompt([
-//         {
-//             type: 'list'
-//         }
-//     ])
-// }
+const userPrompt = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "options",
+        message: "What would you like to do?",
+        choices: [
+          "View all employees",
+          "Add employee",
+          "Update employee role",
+          "Update employee manager",
+          "Delete employee",
+          "View all roles",
+          "Add role",
+          "Delete role",
+          "View all departments",
+          "Add department",
+          "Delete department",
+          "View employees by department",
+          "View department budget",
+          "Quit",
+        ],
+      },
+    ])
+    .then((ans) => {
+      const { choices } = ans;
+
+      if (choices == "View all employees") {
+        showEmployee();
+      }
+
+      if (choices == "Add employee") {
+        addEmployee();
+      }
+
+      if (choices == "Update employee role") {
+        updateRole();
+      }
+
+      if (choices == "Update employee manager") {
+        updateManager();
+      }
+
+      if (choices == "Delete employee") {
+        deleteEmployee();
+      }
+
+      if (choices == "View all roles") {
+        showRole();
+      }
+
+      if (choices == "Add role") {
+        addRole();
+      }
+
+      if (choices == "Delete role") {
+        deleteRole();
+      }
+
+      if (choices == "View all departments") {
+        viewDepartment();
+      }
+
+      if (choices == "Add department") {
+        addDepartment();
+      }
+
+      if (choices == "Delete department") {
+        deleteDepartment();
+      }
+
+      if (choices == "View employees by department") {
+        showDepartmentEmployee();
+      }
+
+      if (choices == "View department budget") {
+        showBudget();
+      }
+
+      if (choices == "Quit") {
+        db.end();
+      }
+    });
+};
